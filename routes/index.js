035b674ec18be6d1b20f677e9e5cf2b9b769e242
alexkollan <finapocalypse@gmail.com>
2020-04-03 19:07:09 +0300

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const csv = require('fast-csv');
+
+function getRawData() {
+  let localParsed = [];
+  return new Promise((res, rej) => {
+    // console.log('promise:', localParsed.length)
+    fs.createReadStream(path.resolve(__dirname, '../data', 'covid-confirmed-global.csv'))
+      .pipe(csv.parse({headers: true}))
+      .on('error', error => rej(error))
+      .on('data', data => localParsed.push(data))
+      .on('end', function () {
+        console.log('before', localParsed.length)
+        res(localParsed);
+      });
+  });
+}
+
+
+router.get('/alex', async function (req, res, next) {
+  let result;
+  try {
+    result = await getRawData();
+    console.log('res:', result.length)
+    let greeceData = result.find(el => el['Country/Region'] === 'Greece');
+    console.log(greeceData)
+    res.render('index', {data: greeceData});
+  } catch (e) {
+    console.log(e)
+  }
+});
+
+module.exports = router;
