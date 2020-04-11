77e6dd5759c3c5dee79cd388959fe46fd58d2590
alexkollan <finapocalypse@gmail.com>
2020-04-11 16:35:39 +0300

    chartistjs implementation and first graph

--- public/javascripts/drawGraph.js
+++ public/javascripts/drawGraph.js
@@ -25,6 +25,40 @@ function drawChart(data) {
     P.S: sou exw ftiaksei kai ena element sto index.ejs to opoio einai starting point sou
     <div id="graph-container" class="container"></div>
     */
-   
+   console.log(data)
+    let date = data.map(function(d){
+        return d.date
+        // .substring(0, d.date.length - 3);
+    })
+    console.log(date)
+    let pNumber = data.map(function(d){
+        return parseInt(d.num);
+    })
+    let num = [];
+    num.push(pNumber)
+   console.log(num)
 
+   let graphData = {
+    labels: date,
+    series: num
+   }
+   let graphOptions = {
+    fullWidth: true,
+    chartPadding: {
+      right: 40
+    },
+    axisX: {
+        labelInterpolationFnc: function skipLabels(value, index, labels) {
+          let labelScale = Math.round( ( labels.length + 60 ) / 30 );
+          console.log(labelScale)
+          if(labels.length > 10) {
+            return index % 10  === 0 ? value : null;
+          } else {
+            return value;
+          }
+        }
+      }
+   }
+
+  new Chartist.Line('#graph-container', graphData, graphOptions);
 }
